56f06f8c535e8a18a2df93d94a474a1fc5ee1145
riskymind <oparaiskelechi@gmail.com>
2020-03-24 15:37:33 +0100

    done with designing all the components

---
+++ src/components/TodoInput.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export class TodoInput extends Component {
+  render() {
+    return (
+      <div className="card card-body my-3">
+        <form>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fa fa-book" />
+              </div>
+            </div>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Add a todo item"
+            />
+          </div>
+          <button className="btn btn-block btn-primary mt-3" type="submit">
+            Add Item
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TodoInput;
